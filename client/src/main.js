0ca853db9bd9afa952d884c92bde76b562be2ae9
Sergio Morales <sergio@cornershopapp.com>
2019-11-08 11:04:48 -0300

    fix(datetime): 24H format
    Signed-off-by: Sergio Morales <sergio@cornershopapp.com>

--- client/src/main.js
+++ client/src/main.js
@@ -9,7 +9,7 @@ Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.filter("formatDate", function(value) {
   if (value) {
-    return moment(String(value)).format("DD/MM/YYYY hh:mm a");
+    return moment(String(value)).format("DD/MM/YYYY HH:mm");
   }
 });
 Vue.filter("formatCron", function(value) {
